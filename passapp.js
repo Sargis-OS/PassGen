39a195d18b1af5b2a026fe7d9ee75961dd8c5e8f
Seggy0s <sargisshawel8@gmail.com>
2019-10-07 03:38:09 -0500

    stillnotdone

--- passapp.js
+++ passapp.js
@@ -1,7 +1,62 @@
 var pw = [];
-var letters = ["abcdefghijklmnopqrstuvwxyz"];
-var symbols = ["!$@#%&*^"];
-var numbers = ["0123456789"];
+var uLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+var dLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var pwLen = prompt("what is you pw length");
 var isSymbols = true;
 var isNumbers = true;
@@ -15,7 +70,7 @@ function shuffle(array) {
   while (0 !== currentIndex) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+    currentIndex = 1;
 
     // And swap it with the current element.
     temporaryValue = array[currentIndex];
@@ -28,6 +83,11 @@ function shuffle(array) {
 
 /* defined function */
 function createPassword() {
+  var pw = [];
+  if (pwLen > 128) {
+    pwLen = prompt("pw must be less than 128 char");
+    createPassword();
+  }
   for (i = 0; i < pwLen; i++) {
     if (isSymbols === true && i === 0) {
       var randomSymNo = Math.floor(Math.random() * symbols.length);
@@ -35,13 +95,13 @@ function createPassword() {
       pw.push(symbols[randomSymNo]);
     } else {
       /* inject a letter into the string */
-      var randomNo = Math.floor(Math.random() * letters.length);
-      pw.push(letters[randomNo]);
+      var randomNo = Math.floor(Math.random() * dLetters.length);
+      pw.push(dLetters[randomNo]);
     }
   }
   /* randomize array */
   pwShuffle = shuffle(pw);
-  pwBox.innerHTML = pwShuffle.toString();
+  pwBox.innerHTML = pwShuffle.join("");
 }
 
 /* first event */
